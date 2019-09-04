5f11f983473ab514dca92ee03ba0e476729670ae
monstruSSG <b_giurgi@yahoo.ro>
2019-09-04 20:58:42 +0300

    Merged

--- nodejs/src/sockets/invitation/invitation.js
+++ nodejs/src/sockets/invitation/invitation.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const wordLogic = require('../../api/v1/word/logic')
-<<<<<<< Updated upstream
-=======
-const usersLogic = require('../../api/v1/user/logic');
 const { busy } = require('../../utils/constants/app');
->>>>>>> 007982d8f1e0ad5e43c54678676088d3a8ff2e26
-=======
-const usersLogic = require('../../api/v1/user/logic')
->>>>>>> Stashed changes
+const usersLogic = require('../../api/v1/user/logic');
 
 
 module.exports = (io, socket) => {
@@ -20,23 +13,21 @@ module.exports = (io, socket) => {
     socket.on('invitationAccepted', data => {
         console.log(`Invitation accepted by: ${socket.id}`);
 
-<<<<<<< Updated upstream
-<<<<<<< HEAD
         //send 'startGame' to both players, but only one
         //who was invited gets the starting word
         wordLogic.getRandomValidWord().then(word => {
             io.to(data.socketId).emit('startGame', { socketId: socket.id })
             io.to(socket.id).emit('startGame', { socketId: data.socketId, word })
         })
-=======
+
         //send 'startGame' to both players
         io.to(data.socketId).emit('startGame', { socketId: socket.id });
         io.to(socket.id).emit('startGame', { socketId: data.socketId });
-=======
+
         //send 'startGame' to both players
         io.to(data.socketId).emit('startGame', { socketId: socket.id, turn: false });
         io.to(socket.id).emit('startGame', { socketId: data.socketId, turn: true });
->>>>>>> Stashed changes
+
 
         //set users as being busy
         Promise.all([
@@ -51,10 +42,6 @@ module.exports = (io, socket) => {
                 }
             )
         ])
-<<<<<<< Updated upstream
->>>>>>> 007982d8f1e0ad5e43c54678676088d3a8ff2e26
-=======
->>>>>>> Stashed changes
     })
 
     socket.on('invitationDeclined', data => {
